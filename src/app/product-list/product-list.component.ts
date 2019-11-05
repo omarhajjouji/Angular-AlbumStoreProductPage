39b1c7e18ec6112a701ba3e4d4fb6848f7f04c3e
omarhajjouji <omarhajjouji@gmail.com>
2019-11-05 16:10:16 +0100

    f

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,7 +8,7 @@ import {ProductService} from '../product.service'
 })
 export class ProductListComponent implements OnInit {
   products : Product[];
-  constructor(private _productService:ProductService) { }
+  constructor(private _productService:ProductService)  { }
 
   ngOnInit() {
     this._productService.getProducts().subscribe(response => this.products = response)
