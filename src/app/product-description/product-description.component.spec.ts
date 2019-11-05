042b229ca5dfc1123f633991c099f96b2c30e9a1
omarhajjouji <omarhajjouji@gmail.com>
2019-11-05 14:57:02 +0100

    add

--- src/app/product-description/product-description.component.spec.ts
+++ src/app/product-description/product-description.component.spec.ts
@@ -2,6 +2,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductDescriptionComponent } from './product-description.component';
 
+
+
+
 describe('ProductDescriptionComponent', () => {
   let component: ProductDescriptionComponent;
   let fixture: ComponentFixture<ProductDescriptionComponent>;
