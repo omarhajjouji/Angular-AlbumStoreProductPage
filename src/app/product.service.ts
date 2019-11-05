9bf26c5c08f6d0f99941bd22e2a0d5827bafd24c
omarhajjouji <omarhajjouji@gmail.com>
2019-11-05 16:02:03 +0100

    d

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -9,6 +9,7 @@ import {Product} from './product'
 export class ProductService {
   private _albumUrl :string =  '../assets/album.json';
   private _productsUrl :string = '../assets/products.json'
+
   constructor(private _http: Http) { }
   
   getAlbum(id:number): Observable<Album> {
@@ -16,7 +17,7 @@ export class ProductService {
   
   }
 
-  getProducts(): Observable<Product[]>{
+  getProducts(): Observable<Product[]> {
     return this._http.get(this._productsUrl).map(response => <Product[]> response.json())
   }
 }
